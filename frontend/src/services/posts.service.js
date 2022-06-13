a4641b8a7b6ff81e5b1c88d6a74efe58863b2161
angelinekaren <71621472+angelinekaren@users.noreply.github.com>
2022-06-14 01:45:59 +0700

    modify nodeflux

--- frontend/src/services/posts.service.js
+++ frontend/src/services/posts.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+require("dotenv").config();
 
 async function faceMatch(image1, image2) {
   const data = JSON.stringify({
@@ -7,7 +8,13 @@ async function faceMatch(image1, image2) {
 
   // console.log(data);
 
-  return await axios.post("/face-match-liveness", data);
+  return await axios.post("/face-match-liveness", data, {
+    headers: {
+      Authorization: `NODEFLUX-HMAC-SHA256 Credential=${process.env.NODEFLUX_ACCESS_KEY}/${process.env.NODEFLUX_DATE}/nodeflux.api.v1beta1.ImageAnalytic/StreamImageAnalytic, SignedHeaders=x-nodeflux-timestamp, Signature=${process.env.NODEFLUX_TOKEN}`,
+      "x-nodeflux-timestamp": `${process.env.X_NODEFLUX_TIMESTAMP}`,
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 async function getListing(id) {
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,4 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
 
 module.exports = function (app) {
   app.use(
@@ -9,15 +8,9 @@ module.exports = function (app) {
     })
   );
   app.use(
-    "/face-match-liveness",
-    createProxyMiddleware({
+    createProxyMiddleware("/face-match-liveness", {
       target: "https://api.cloud.nodeflux.io/v1/analytics",
       changeOrigin: true,
-      headers: {
-        Authorization: `NODEFLUX-HMAC-SHA256 Credential=${process.env.NODEFLUX_ACCESS_KEY}/${process.env.NODEFLUX_DATE}/nodeflux.api.v1beta1.ImageAnalytic/StreamImageAnalytic, SignedHeaders=x-nodeflux-timestamp, Signature=${process.env.NODEFLUX_TOKEN}`,
-        "x-nodeflux-timestamp": `${process.env.X_NODEFLUX_TIMESTAMP}`,
-        "Content-Type": "application/json",
-      },
     })
   );
   app.use(
