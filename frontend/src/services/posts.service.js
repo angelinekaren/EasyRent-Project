213d8f9af4ebb8c72ebaac16ac7936f8978290a8
angelinekaren <71621472+angelinekaren@users.noreply.github.com>
2022-06-14 10:57:56 +0700

    trying again

--- frontend/src/services/posts.service.js
+++ frontend/src/services/posts.service.js
@@ -5,7 +5,17 @@ async function faceMatch(image1, image2) {
     images: [image1, image2],
   });
 
-  return await axios.post("/face-match-liveness", data);
+  return await axios.post(
+    "https://api.cloud.nodeflux.io/v1/analytics/face-match-liveness/face-match-liveness",
+    data,
+    {
+      headers: {
+        Authorization: `NODEFLUX-HMAC-SHA256 Credential=${process.env.NODEFLUX_ACCESS_KEY}/${process.env.NODEFLUX_DATE}/nodeflux.api.v1beta1.ImageAnalytic/StreamImageAnalytic, SignedHeaders=x-nodeflux-timestamp, Signature=${process.env.NODEFLUX_TOKEN}`,
+        "x-nodeflux-timestamp": `${process.env.X_NODEFLUX_TIMESTAMP}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
 
 async function getListing(id) {
